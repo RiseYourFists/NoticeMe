1a27568787cbdad0cdaf297e1a60e958f68d0702
Emanuel Shavepeyer <daru12378@gmail.com>
2025-06-18 22:55:59 +0300

    Add project files.

---
+++ NoticeMe.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoticeMe.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
